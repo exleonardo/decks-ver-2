0a06c78930c10eca88c0cfb716a07ed68406a3d2
exleonardo <exleonardo@mail.ru>
2023-10-24 21:11:44 +0300

    add generic

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,14 +4,30 @@ import { GlobalError } from './GlobalError/GlobalError.tsx'
 import { useAppSelector } from './store.ts'
 import { LinearLoader } from '../common/components/Loader/LinearLoader.tsx'
 import { selectAppStatus } from './app-selectors.ts'
+import * as React from 'react'
+import { ReactNode } from 'react'
 
 export const App = () => {
 const status = useAppSelector(selectAppStatus)
   return (
     <div>
+      <List item={['HTML',"CSS"]} renderItem={(item:string)=>item.toUpperCase()}/>
+      <List item={[1,2,4]} renderItem={(item:number)=>item.toFixed()}/>
       {status==='loading' &&<LinearLoader/>}
       <Decks />
       <GlobalError />
     </div>
+  )
+}
+
+type ListType<D> = {
+  item:D[]
+  renderItem:(item:D)=>ReactNode
+}
+function List <D>(props:ListType<D>){
+  return (
+    <ul>
+      {props.item.map((el,index)=> <li key={index}>{props.renderItem(el)}</li>)}
+    </ul>
   )
 }
---
+++ src/tasks.ts
@@ -0,0 +1,3 @@
+export const getLastItem = <D>(array:Array<D>): D=>{
+  return array[array.length-1]
+}
